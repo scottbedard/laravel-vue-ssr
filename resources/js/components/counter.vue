04a7ed411b415a7924165dec532776a1d1599c31
Scott Bedard <scottbedard90@gmail.com>
2024-02-04 17:55:44 -0500

    scaffolding

---
+++ resources/js/components/counter.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <button @click="increment">count: {{ count }}</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>laravel-vue-ssr</title>
+        @vite([
+            'resources/js/app.js',
+        ])
     </head>
     <body class="antialiased">
         <x-vue />
